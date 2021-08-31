78bbe5de59c489834a77479653f3c27e7d5ccff9
HongBryan <185078974@qq.com>
2021-08-31 15:28:08 +0800

    Add files via upload

---
+++ BaseJsonModel.h
@@ -0,0 +1,18 @@
+//
+//  BaseJsonModel.h
+//  BaseJsonModel
+//
+//  Created by Bryan on 2021/8/31.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BaseJsonModel.
+FOUNDATION_EXPORT double BaseJsonModelVersionNumber;
+
+//! Project version string for BaseJsonModel.
+FOUNDATION_EXPORT const unsigned char BaseJsonModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaseJsonModel/PublicHeader.h>
+
+
---
+++ BaseJsonModel.swift
@@ -0,0 +1,12 @@
+//
+//  BaseJsonModel.swift
+//  BaseJsonModel
+//
+//  Created by Bryan on 2021/8/31.
+//
+
+import Foundation
+
+func test() {
+    
+}
